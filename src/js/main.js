20369594caeb32910d7f248120753e3e433df57d
Val_F4 <vallkemgamer@gmail.com>
2024-08-07 14:16:53 -0300

    Config home page

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,5 @@
-let menuHamburguer = document.querySelector('img#icon_navegation')
+let hamburguerMenu = document.querySelector('img#icon_navegation')
 let menuNav = document.querySelector('#navegation')
-let sectionDados = document.querySelector('#Inicio')
-let contCafes = document.querySelector('#coffee_number')
-let contAtendidos = document.querySelector('#clientes_number')
-let contIdeias = document.querySelector('#ideias_number')
-
-function contator(contadorElement, valorAlvo) {
-    let contador = 0;
-    let intervalo = 1;
-
-    function atualizar() {
-      contadorElement.textContent = `+ de ${contador}`;
-
-      if (contador < valorAlvo) {
-        contador++;
-        if (contador >= valorAlvo - 50) {
-          intervalo = 10;
-        }
-        setTimeout(atualizar, intervalo);
-      }
-    }
-    atualizar();
-  }
 
 function click_menu() {
     if (options.style.display == 'flex') {
@@ -36,35 +14,17 @@ function click_menu() {
     }
 }
 
-function upgrate_windows() {
+function upgrade_windows() {
     if (window.innerWidth >= 769) {
         options.style.display = 'flex'
     }
-    else {
-        options.style.display = 'none'
-    }
+
+    options.style.display = 'none'
 }
 
-function open_menu() {
-    menuNav.classList.toggle(`rolagem`, window.scrollY > 100)
-}
+hamburguerMenu.addEventListener("click", click_menu)
 
-function verificarVisibilidade() {
-    const observer = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting) {
-        sectionDados.classList.add('background_infos')
-        contator(contAtendidos, 150);
-        contator(contCafes, 500);
-        contator(contIdeias, 1000);
-        
-        observer.disconnect();
-      }
-    });
+window.addEventListener('scroll', function() {
+  menuNav.classList.toggle(`rolagem`, window.scrollY > 100)
+})
 
-    observer.observe(sectionDados);
-}
-
-menuHamburguer.addEventListener("click", click_menu)
-window.addEventListener('scroll', open_menu)
-window.addEventListener('scroll', open_menu)
-document.addEventListener("DOMContentLoaded", verificarVisibilidade)
